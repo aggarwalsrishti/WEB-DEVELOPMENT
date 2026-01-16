c781433209b989a2eaeee9d905e13b03fa0e2605
Srishti Aggarwal <srishtiaggarwal1303@gmail.com>
2026-01-17 04:16:20 +0530

    JavaScript-Introduction

---
+++ 089-INTRODUCTION/script.js
@@ -0,0 +1,13 @@
+alert("Hello World");
+console.log("Code is running");
+console.log("Code still running");
+console.log("Coding is great");
+var a=prompt("Enter your number");
+var isTrue=confirm("Do you want to leave");
+if(isTrue){
+    console.log("Leaving");
+}
+else{
+    console.log("Staying");
+}
+console.log("Your number is "+a)
